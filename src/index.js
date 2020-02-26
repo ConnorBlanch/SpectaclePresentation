c1729ba37cdba65eb04c439a379f1e79d1d0190f
Connor Blanch <connorblanch@gmail.com>
2020-02-25 22:44:09 -0500

    Finished most of the slides

--- src/index.js
+++ src/index.js
@@ -51,6 +51,38 @@ const template = () => (
 );
 // SPECTACLE_CLI_TEMPLATE_END
 
+const slideCodeBlock = `<Slide transitionEffect="slide">
+		<FlexBox height="15%" flexDirection="row" justifyContent="center">
+			<Text color="secondary" fontSize="80px">
+				Courses to Take
+			</Text>
+		</FlexBox>
+		<FlexBox height="85%" flexDirection="column" alignItems="flex-start">
+			<UnorderedList fontSize="20px">
+				<Text color="secondary" fontSize="25px">
+					Pre College
+				</Text>
+				<ListItem>Mathematics</ListItem>
+				<ListItem>Science</ListItem>
+				<ListItem>Any programming type classes that are available</ListItem>
+				<ListItem>Online!</ListItem>
+				<Text color="secondary" fontSize="25px">
+					College
+				</Text>
+				<ListItem>Mathematics</ListItem>
+				<ListItem>Pre Defined Computer Courses</ListItem>
+			</UnorderedList>
+		</FlexBox>
+		<Image
+			src="https://cdn.promessinc.com/wp-content/themes/promess/images/Promess-Incorporated.png"
+			width="180"
+			height="auto"
+			alt="Company Logo"
+			align="left"
+		/>
+	</Slide>
+`;
+
 const Presentation = () => (
 	<Deck theme={theme} template={template} transitionEffect="fade">
 		<Slide transitionEffect="slide">
@@ -104,13 +136,146 @@ const Presentation = () => (
 		<Slide transitionEffect="slide">
 			<FlexBox height="25%" flexDirection="row" justifyContent="center">
 				<Text color="secondary" fontSize="80px">
-					Great Things About Development/
+					Things I Love About My Job
 				</Text>
 			</FlexBox>
 			<FlexBox height="75%" flexDirection="column" alignItems="flex-start">
 				<UnorderedList fontSize="30px">
-					<ListItem />
+					<ListItem>New challenges each day</ListItem>
+					<ListItem>Extreme problem solving</ListItem>
+					<ListItem>Working with other departments to provide a great product</ListItem>
+					<ListItem>Leading our Software Department</ListItem>
+					<ListItem>Helping my team achieve their personal and work goals</ListItem>
 				</UnorderedList>
+			</FlexBox>
+			<Image
+				src="https://cdn.promessinc.com/wp-content/themes/promess/images/Promess-Incorporated.png"
+				width="180"
+				height="auto"
+				alt="Company Logo"
+				align="left"
+			/>
+		</Slide>
+		<Slide transitionEffect="slide">
+			<FlexBox height="25%" flexDirection="row" justifyContent="center">
+				<Text color="secondary" fontSize="80px">
+					Work Environment
+				</Text>
+			</FlexBox>
+			<FlexBox height="75%" flexDirection="column" alignItems="flex-start">
+				<UnorderedList fontSize="30px">
+					<ListItem>Pretty Quiet</ListItem>
+					<ListItem>Open Concept</ListItem>
+					<ListItem>Decorating for Holidays!</ListItem>
+					<ListItem>Laid Back</ListItem>
+				</UnorderedList>
+			</FlexBox>
+			<Image
+				src="https://cdn.promessinc.com/wp-content/themes/promess/images/Promess-Incorporated.png"
+				width="180"
+				height="auto"
+				alt="Company Logo"
+				align="left"
+			/>
+		</Slide>
+		<Slide transitionEffect="slide">
+			<FlexBox height="25%" flexDirection="row" justifyContent="center">
+				<Text color="secondary" fontSize="80px">
+					Why Computer Science/Engineering
+				</Text>
+			</FlexBox>
+			<FlexBox height="75%" flexDirection="column" alignItems="flex-start">
+				<UnorderedList fontSize="30px">
+					<ListItem>High School Programming Class</ListItem>
+					<ListItem>Loved Computers Already</ListItem>
+					<ListItem>Enjoyed the idea of creating something that someone could easily access</ListItem>
+				</UnorderedList>
+			</FlexBox>
+			<Image
+				src="https://cdn.promessinc.com/wp-content/themes/promess/images/Promess-Incorporated.png"
+				width="180"
+				height="auto"
+				alt="Company Logo"
+				align="left"
+			/>
+		</Slide>
+		<Slide transitionEffect="slide">
+			<FlexBox height="25%" flexDirection="row" justifyContent="center">
+				<Text color="secondary" fontSize="80px">
+					Similar Careers
+				</Text>
+			</FlexBox>
+			<FlexBox height="75%" flexDirection="column" alignItems="flex-start">
+				<UnorderedList fontSize="30px">
+					<ListItem>Software Developer</ListItem>
+					<ListItem>Quality Assurance Engineer</ListItem>
+					<ListItem>UI/UX Developer</ListItem>
+					<ListItem>Software Manager/Team Lead</ListItem>
+					<ListItem>Product Development</ListItem>
+				</UnorderedList>
+			</FlexBox>
+			<Image
+				src="https://cdn.promessinc.com/wp-content/themes/promess/images/Promess-Incorporated.png"
+				width="180"
+				height="auto"
+				alt="Company Logo"
+				align="left"
+			/>
+		</Slide>
+		<Slide transitionEffect="slide">
+			<FlexBox height="15%" flexDirection="row" justifyContent="center">
+				<Text color="secondary" fontSize="80px">
+					Courses to Take
+				</Text>
+			</FlexBox>
+			<FlexBox height="85%" flexDirection="column" alignItems="flex-start">
+				<UnorderedList fontSize="20px">
+					<Text color="secondary" fontSize="25px">
+						Pre College
+					</Text>
+					<ListItem>Mathematics</ListItem>
+					<ListItem>Science</ListItem>
+					<ListItem>Any programming type classes that are available</ListItem>
+					<ListItem>Online!</ListItem>
+					<Text color="secondary" fontSize="25px">
+						College
+					</Text>
+					<ListItem>Mathematics</ListItem>
+					<ListItem>Pre Defined Computer Courses</ListItem>
+				</UnorderedList>
+			</FlexBox>
+			<Image
+				src="https://cdn.promessinc.com/wp-content/themes/promess/images/Promess-Incorporated.png"
+				width="180"
+				height="auto"
+				alt="Company Logo"
+				align="left"
+			/>
+		</Slide>
+		<Slide transitionEffect="slide">
+			<FlexBox height="100%" flexDirection="column" alignItems="center">
+				<Text color="secondary" fontSize="80px">
+					Live Demo! Can You Guess What It Is?
+				</Text>
+			</FlexBox>
+			<Image
+				src="https://cdn.promessinc.com/wp-content/themes/promess/images/Promess-Incorporated.png"
+				width="180"
+				height="auto"
+				alt="Company Logo"
+				align="left"
+			/>
+		</Slide>
+		<Slide transitionEffect="slide">
+			<FlexBox height="25%" flexDirection="row" justifyContent="center">
+				<Text color="secondary" fontSize="80px">
+					This Presentation Was Done With Code!
+				</Text>
+			</FlexBox>
+			<FlexBox height="75%" flexDirection="column" alignItems="flex-start">
+				<CodePane fontSize={25} language="JavaScript" autoFillHeight>
+					{slideCodeBlock}
+				</CodePane>
 			</FlexBox>
 			<Image
 				src="https://cdn.promessinc.com/wp-content/themes/promess/images/Promess-Incorporated.png"
@@ -124,45 +289,7 @@ const Presentation = () => (
 			<FlexBox height="25%" flexDirection="row" justifyContent="center">
 				<Text color="secondary" fontSize="80px" />
 			</FlexBox>
-			<FlexBox height="75%" flexDirection="column" alignItems="flex-start">
-				<UnorderedList fontSize="30px">
-					<ListItem />
-				</UnorderedList>
-			</FlexBox>
-			<Image
-				src="https://cdn.promessinc.com/wp-content/themes/promess/images/Promess-Incorporated.png"
-				width="180"
-				height="auto"
-				alt="Company Logo"
-				align="left"
-			/>
-		</Slide>
-		<Slide transitionEffect="slide">
-			<FlexBox height="25%" flexDirection="row" justifyContent="center">
-				<Text color="secondary" fontSize="80px" />
-			</FlexBox>
-			<FlexBox height="75%" flexDirection="column" alignItems="flex-start">
-				<UnorderedList fontSize="30px">
-					<ListItem />
-				</UnorderedList>
-			</FlexBox>
-			<Image
-				src="https://cdn.promessinc.com/wp-content/themes/promess/images/Promess-Incorporated.png"
-				width="180"
-				height="auto"
-				alt="Company Logo"
-				align="left"
-			/>
-		</Slide>
-		<Slide transitionEffect="slide">
-			<FlexBox height="25%" flexDirection="row" justifyContent="center">
-				<Text color="secondary" fontSize="80px" />
-			</FlexBox>
-			<FlexBox height="75%" flexDirection="column" alignItems="flex-start">
-				<UnorderedList fontSize="30px">
-					<ListItem />
-				</UnorderedList>
-			</FlexBox>
+			<FlexBox height="75%" flexDirection="column" alignItems="flex-start" />
 			<Image
 				src="https://cdn.promessinc.com/wp-content/themes/promess/images/Promess-Incorporated.png"
 				width="180"
